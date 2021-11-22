fab727b87ee479ab9c3d9f197b1b2de271ea9e4d
adaliz <Cadaliz@LES008134.corp.capgemini.com>
2021-11-22 09:10:02 +0100

    primer proyecto de spring boot api rest

---
+++ springboot-apirest/src/main/java/com/example/demo/models/dao/ClienteDao.java
@@ -0,0 +1,9 @@
+package com.example.demo.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.models.entity.Cliente;
+
+public interface ClienteDao extends CrudRepository<Cliente, Long>{//IMPLEMENTA METODOS DE MANIPULACION
+
+}
